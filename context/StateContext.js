9317e6b0f944c84c2377d68d5898d4165f5049cf
Dani Portela <daniel.portela96@hotmail.com>
2022-12-30 16:38:39 -0300

    Implement context for managing cart logic globally.

---
+++ context/StateContext.js
@@ -0,0 +1,68 @@
+import React, { createContext, useContext, useState, useEffect } from "react"
+
+import { toast } from "react-hot-toast"
+
+const Context = createContext()
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false)
+    const [cartItems, setCartItems] = useState([])
+    const [totalPrice, setTotalPrice] = useState()
+    const [totalQuantities, setTotalQuantities] = useState(0)
+    const [qty, setQty] = useState(1)
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find(item => item._id === product._id)
+
+        setTotalPrice((previousTotalPrice) => previousTotalPrice + product.price * quantity)
+        setTotalQuantities((previousTotalQuantities) => previousTotalQuantities + quantity)
+
+        if (checkProductInCart) {
+            const updatedCartItems = cartItems.map(item => {
+                if (item._id === product._id) return {
+                    ...item,
+                    quantity: item.quantity + quantity
+                }
+            })
+            setCartItems(updatedCartItems)
+        } else {
+            product.quantity = quantity
+            
+            setCartItems([...cartItems, { ...product }])
+        }
+
+        toast.success(`${qty} ${product.name} added to the cart`)
+    }
+
+    const increaseQty = () => {
+        setQty((prevQty) => prevQty + 1)
+    }
+
+    const decreaseQty = () => {
+        setQty((prevQty) => {
+            if (prevQty - 1 < 1) return 1
+
+            return prevQty - 1
+        })
+    }
+
+    return (
+        <Context.Provider
+            value={{
+                showCart,
+                cartItems,
+                totalPrice,
+                totalQuantities,
+                qty,
+                increaseQty,
+                decreaseQty,
+                onAdd,
+                setShowCart
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(Context)
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Product } from "../../components"
+import { useStateContext } from "../../context/StateContext"
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai"
 
@@ -8,6 +9,7 @@ import { client, urlFor } from "../../lib/client";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0)
+  const { decreaseQty, increaseQty, qty, onAdd } = useStateContext()
 
   return (
     <div>
@@ -40,13 +42,13 @@ const ProductDetails = ({ product, products }) => {
             <div className="quantity">
                 <h3>Quantity:</h3>
                 <p className="quantity-desc">
-                    <span className="minus" onClick=""><AiOutlineMinus /></span>
-                    <span className="num" onClick="">0</span>
-                    <span className="plus" onClick=""><AiOutlinePlus /></span>
+                    <span className="minus" onClick={decreaseQty}><AiOutlineMinus /></span>
+                    <span className="num" onClick="">{qty}</span>
+                    <span className="plus" onClick={increaseQty}><AiOutlinePlus /></span>
                 </p>
             </div>
             <div className="buttons">
-                <button type="button" className="add-to-cart" onClick="">Add to cart</button>
+                <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to cart</button>
                 <button type="button" className="buy-now" onClick="">Buy now</button>
             </div>
         </div>
