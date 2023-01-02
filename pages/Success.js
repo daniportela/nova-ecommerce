5d96228c174eec674c6a87db318e00276f74a2f4
Dani Portela <daniel.portela96@hotmail.com>
2023-01-02 16:51:06 -0300

    Create Success page.

---
+++ pages/Success.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useRouter } from "next/router";
+
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+  const [order, setOrder] = useState(null);
+
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
